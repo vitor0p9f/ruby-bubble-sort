f6a27ddb4661523576dff0ca0b64690fb2999d90
vitor0p9f <dev.vitorpfontes002@gmail.com>
2024-01-16 14:31:08 -0300

    test(spec/bubble_sort.spec.rb): Writing a red test
    - Checks if 'read_file_numbers' function can read numbers of a single line file, that contains numbers, letters and symbols.

--- spec/bubble_sort.spec.rb
+++ spec/bubble_sort.spec.rb
@@ -10,5 +10,11 @@ RSpec.describe 'Bubble sort project' do
 
       expect(content).to eq [1, 2, 3, 4, 5]
     end
+
+    it 'should read numbers of a file with a single line that contains numbers, letters and symbols' do
+      content = read_file_numbers('test-one-line-not-only-numbers.txt')
+
+      expect(content).to eq [6, 8, 9, 0]
+    end
   end
 end
