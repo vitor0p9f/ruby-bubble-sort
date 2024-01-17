bfe80505c72bc72cd7379ea6928195e7994912b4
vitor0p9f <dev.vitorpfontes002@gmail.com>
2024-01-17 16:07:22 -0300

    feat(bubble_sort.rb): Making a new function
    - Making a 'main' function that reads data from the input file, sorts it using the bubble sort algorithm and writes the result to an output file. File names are obtained via command line parameters.

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -27,3 +27,20 @@ def bubble_sort(array)
 
   array
 end
+
+def main
+  if ARGV.size < 2
+    puts 'This program requires 2 parameters. The name of the file that contains the numbers to be sorted and the name of the file where the sorting result will be written.'
+    puts
+    puts 'Example: ruby bubble_sort.rb input.txt output.txt'
+
+    return
+  end
+  file_content = read_file_numbers(ARGV[0])
+
+  sorted_numbers = bubble_sort(file_content)
+
+  File.write(ARGV[1], sorted_numbers.join(' '), mode: 'w')
+end
+
+main
