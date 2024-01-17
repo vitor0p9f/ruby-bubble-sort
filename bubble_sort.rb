e50140b3a3a40a757bb94a5726ac61a3b06ab3bd
vitor0p9f <dev.vitorpfontes002@gmail.com>
2024-01-17 14:22:05 -0300

    feat(bubble_sort.rb): Making a new function
    - Making the 'bubble_sort' function, that sorts an array of numbers.
    - This function made the 'should sort numbers' test pass.

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -10,3 +10,20 @@ def read_file_numbers(file_name)
     .split(' ')
     .map(&:to_i)
 end
+
+def bubble_sort(array)
+  loop do
+    swaped = false
+
+    (array.size - 1).times do |index|
+      next unless array[index] > array[index + 1]
+
+      array[index], array[index + 1] = array[index + 1], array[index]
+      swaped = true
+    end
+
+    break unless swaped
+  end
+
+  array
+end
